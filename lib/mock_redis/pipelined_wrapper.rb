0b2e7bf1fc5a5edea5d4482db044801209d2fde2
Jakub Wroblewski <jakub.wroblewski@madvertise.com>
2013-06-02 14:41:12 +0200

    Make pipelined cmds return immediately

--- lib/mock_redis/pipelined_wrapper.rb
+++ lib/mock_redis/pipelined_wrapper.rb
@@ -8,38 +8,19 @@ class MockRedis
 
     def initialize(db)
       @db = db
-      @pipelined_commands = []
-      @in_pipeline = false
     end
 
     def initialize_copy(source)
       super
       @db = @db.clone
-      @pipelined_commands = @pipelined_commands.clone
     end
 
     def method_missing(method, *args, &block)
-      if @in_pipeline
-        @pipelined_commands << [method, *args]
-        nil
-      else
-        @db.send(method, *args, &block)
-      end
+      Struct.new(:value).new( @db.send(method, *args, &block) )
     end
 
     def pipelined(options = {})
-      @in_pipeline = true
       yield
-      @in_pipeline = false
-      responses = @pipelined_commands.map do |cmd|
-        begin
-          send(*cmd)
-        rescue => e
-          e
-        end
-      end
-      @pipelined_commands = []
-      responses
     end
   end
 end
